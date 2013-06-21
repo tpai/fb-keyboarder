b23a2f4f777591a5046e3cac9e5fef5f27df2eec
tonypai <tony77794@gmail.com>
2013-06-21 21:09:20 +0800

    v1.6 * 重構 from native js to jquery * 聊天快捷功能失效 >>因官方變更classname 已修復

--- target/main.js
+++ target/main.js
@@ -1,316 +1,295 @@
-var msg_notif_i = 0, //msg & notif 索引
-	story_i = 0, //story 索引
-	group_i = 0, //sidebar 索引
-	more_btn, //更多貼文按鈕物件
-	home, //story container
-	sidebar, //group container
-	state = "story" //story or group
-	
-var setContainer = function(state) {
-	if(state == "story") {
-		//如果有兩個以上的uiStreamHomepage必然是社團中的置頂跟一般貼文 預設選擇一般貼文 並且必須時時更新 因為使用者可能會用滑鼠點開其他社團
-		home = (document.getElementsByClassName("uiStreamHomepage").length > 1)?document.getElementsByClassName("uiStreamHomepage")[1]:document.getElementsByClassName("uiStreamHomepage")[0]
-	}
-	else if(state == "group") {
-		if(document.getElementById("pinnedNav") != null) {
-			sidebar = document.getElementById("pinnedNav").getElementsByClassName("uiSideNav")[0]
-			var groups = sidebar.getElementsByClassName("sideNavItem")
-			for(var i=0;i<groups.length;i++) {
-				if(groups[i].className.search("selectedItem") != -1) {
-					group_i = i
-				}
-			}
+//排程工作
+var at, stage
+setInterval(function() {
+	//通知自動增量
+	if ($("#fbNotificationsFlyout").prop("class").search("toggleTargetClosed") == -1) {
+		if((msg_notif_index+1) / $("#fbNotificationsList").children().length >= 0.8) {
+			$(".notifMorePager").find("a")[0].click()
 		}
 	}
-};
-
-window.onload = function() {
-	setContainer("story")
-	setContainer("group")
-};
-
-//畫面移至story處 並點開留言
-var showStory = function(index) {
-	temp = index
-	var story = home.getElementsByClassName("uiStreamStory")[index]
-	if(story != undefined) {
-		var comment = story.getElementsByClassName("comment_link")
-		if(comment.length > 0)
-			comment[0].getElementsByTagName("input")[0].click()
-		var inputbox = story.getElementsByTagName("textarea")
-		if(inputbox.length > 0)
-			inputbox[0].focus()
-		window.scrollTo(0, story.offsetTop)
+	//訊息自動增量
+	else if ($("#fbMessagesFlyout").prop("class").search("toggleTargetClosed") == -1) {
+		if((msg_notif_index+1) / $(".jewelContent").find("li").length >= 0.8) {
+			$(".uiMorePager").find("a")[0].click()
+		}
 	}
+	//場景判斷
+	if(location.pathname.search("groups") != -1) {
+		at = "group"
+	}
+	else {
+		at = "wall"
+	}
+}, 500)
+
+/* ------------------------------------------------------------------ */
+
+//反選擇
+var blurAll = function() {
+	if ("activeElement" in document)document.activeElement.blur()
+};
+
+//story動作
+var resStory = function(index, action) {
+	var story
+	if(at == "wall")
+		story = $("li[id^='stream_story_']")
+	else if(at == "group")
+		story = $("li[id^='mall_post_']")
+
+	window.scrollTo(0, story[index].offsetTop)
+
+	if(action == "like")
+		$(story[index]).find("a[class*='UFILikeLink']")[0].click()
+	else if(action == "comment")
+		$(story[index]).find("label[class*='comment_link']")[0].click()
 };
 
 //開關對話框
-var chatOnOff = function(offset) {
-	var length = document.getElementById("fbDockChatTabs").childNodes.length
-	var i = length - offset - 1;
-	var tab = document.getElementById("fbDockChatTabs").childNodes[i]
-	if(tab != undefined) {
-		if(tab.className.search("opened") != -1) {
-			tab.getElementsByClassName("titlebar")[0].click()
-		}
-		else {
-			tab.getElementsByTagName("a")[0].click()
-		}
+var chatbox = function(index) {
+	var boxes = $("#ChatTabsPagelet").find("div[class*='videoCallEnabled']").children()
+	if($(boxes[boxes.length-1-index]).prop("class").search("opened") != -1) {
+		console.log("opened")
+		$(boxes[boxes.length-1-index]).find("div[class*='titlebarButtonWrapper']")[0].click()
+	}
+	else {
+		console.log("closed")
+		$(boxes[boxes.length-1-index]).find("a[class*='fbNubButton']")[0].click()
 	}
 };
 
-var tip = function(value) {
-	var ico = document.createElement("div")
-	ico.setAttribute("style", "position: absolute; right: 0px; bottom: 0px; font-size: 10px; line-height: 1; background-color: #3b5998; color: white;")
-	ico.innerHTML = value
-	return ico
-};
+/* ------------------------------------------------------------------ */
 
-document.getElementById("navHome").getElementsByTagName("a")[0].style.lineHeight = "14px"
-document.getElementById("navHome").getElementsByTagName("a")[0].innerHTML += "<br />Alt+1"
-document.getElementsByClassName("headerTinymanName")[0].style.lineHeight = "14px"
-document.getElementsByClassName("headerTinymanName")[0].innerHTML += "<br />Alt+2"
-document.getElementById("fbRequestsJewel").appendChild(tip("Z"))
-document.getElementById("fbMessagesJewel").appendChild(tip("X"))
-document.getElementById("fbNotificationsJewel").appendChild(tip("C"))
-document.getElementById("fbNotificationsList").parentNode.parentNode.style.top = "0px"
-document.getElementById("navSearch").appendChild(tip("V"))
+//全域變數宣告
+var sidebar_index, sidebar_item, story_index, msg_notif_index
+var item, hover, n_item_shown, container
 
 window.onkeydown = function(e) {
-	//console.log(e.which)
-	var trigger, container, children, hover_style, n_shown
-	if(document.getElementById("fbNotificationsFlyout").className.search("toggleTargetClosed") == -1) {
-		container = document.getElementById("fbNotificationsList").parentNode.parentNode
-		children = document.getElementById("fbNotificationsList").childNodes
-		more_btn = document.getElementsByClassName("notifMorePager")[0]
-		hover_style = "notifHover"
-		n_shown = 7
+
+	var jewel
+
+	//通知已開
+	if($("#fbNotificationsFlyout").prop("class").search("toggleTargetClosed") == -1) {
+		item = $("#fbNotificationsList").find("li")
+		hover = "notifHover"
+		n_item_shown = 7
+		container = $("#fbNotificationsList").parent().parent()
 	}
-	else if (document.getElementById("fbMessagesFlyout").className.search("toggleTargetClosed") == -1) {
-		container = document.getElementsByClassName("jewelContent")[0].parentNode.parentNode.parentNode.parentNode;
-		children = document.getElementsByClassName("jewelContent")[0].getElementsByTagName("li")
-		more_btn = document.getElementsByClassName("uiMorePager")[0]
-		hover_style = "jewelItemNew"
-		n_shown = 5
+	//訊息已開
+	else if($("#fbMessagesFlyout").prop("class").search("toggleTargetClosed") == -1) {
+		item = $(".jewelContent").find("li")
+		hover = "jewelItemNew"
+		n_item_shown = 5
+		container = $(".jewelContent").parent().parent().parent().parent()
 	}
 
 	//Ctrl+Alt+
 	if(e.ctrlKey && e.altKey) {
+
 		switch(e.which) {
-			//ESC
+			//ESC : 反選擇
 			case 27:
-				if ("activeElement" in document)document.activeElement.blur()
+				blurAll()
 				break
-			//P
+			//P : 發佈貼文
 			case 80:
-				if(document.getElementById("pagelet_stream_pager") != null)
-					document.getElementById("pagelet_composer").getElementsByTagName("textarea")[0].focus()
+				//首頁
+				if($("#pagelet_stream_pager") == [])
+					$("#pagelet_composer").find("textarea")[0].focus()
+				//社團
 				else
-					document.getElementById("pagelet_group_composer").getElementsByTagName("textarea")[0].focus()
+					$("#pagelet_group_composer").find("textarea")[0].focus()
 				break
-			//A
+			//A : 切換至側邊item並置頂
 			case 65:
-				state = "group"
+				stage = "sidebar"
+				sidebar_index = 0
+				sidebar_item = $("li[id^='navItem_']")
+				$(sidebar_item[0]).css("background-color", "#eff2f7")
+
 				window.scrollTo(0, 0)
-				sidebar.children[group_i].getElementsByClassName("item")[0].style.backgroundColor = "#eff2f7"
 				break
-			//S
+			//S : 切換至story並置頂
 			case 83:
-				state = "story"
-				window.scrollTo(0, 0)
-				showStory(story_i = 0)
+				stage = "wall"
+				story_index = 0
+				resStory(story_index, "comment")
 				break
-			//Z
+			//Z : 快捷鈕 朋友邀請
 			case 90:
-				document.getElementById("fbRequestsJewel").getElementsByTagName("a")[0].click()
+				$("#fbRequestsJewel").find("a")[0].click()
 				break
-			//X
+			//X : 快捷鈕 訊息
 			case 88:
-				msg_notif_i = 0
-				trigger = document.getElementById("fbMessagesJewel").getElementsByTagName("a")[0]
-			//C
+				stage = "msg"
+				jewel = $("#fbMessagesJewel").find("a")[0]
+			//C : 快捷鈕 通知
 			case 67:
-				msg_notif_i = 0
-				//因為msg跟notif可以用相同的程式碼處理 所以利用trigger變數來區分按鈕
-				//疑問: 為何不放上面跟container一起? 因為上面的判斷條件是是否已開啟flyout 沒點按鈕怎麼開flyout呢
-				if(!trigger) trigger = document.getElementById("fbNotificationsJewel").getElementsByTagName("a")[0]
-
-				trigger.click() //按下按鈕
-
-				if ("activeElement" in document)document.activeElement.blur()
-				
-				container.style.top = "0px" //container位移至0
-
-				//清除所有msg(notif)的hover效果
-				for(var j=0;j<children.length;j++) {
-					children[j].className = children[j].className.replace(" "+hover_style, "")
+				//索引置頂
+				msg_notif_index = 0
+				//因為msg跟notif可以用相同的程式碼處理 所以利用jewel來區分
+				if(!jewel) {
+					stage = "notif"
+					jewel = $("#fbNotificationsJewel").find("a")[0]
 				}
+				jewel.click() //按下按鈕
 
-				//在第零個顯示hover效果
-				children[msg_notif_i].className += " "+hover_style
+				//清除所有已選狀態
+				blurAll()
+
+				$(container[0]).css("top", "0px")
+
 				break
-			//D
-			case 68: document.getElementsByClassName("fbChatSidebarMessage")[0].nextSibling.getElementsByClassName("innerWrap")[0].childNodes[0].focus(); break;
-			//V
+			//D : 聊天室搜尋
+			case 68:
+				$(".fbChatSidebarMessage").next().find("input[type='text']")[0].focus()
+				break;
+			//V : 群體搜尋
 			case 86:
-				document.getElementById("q").focus()
+				$("#q").focus()
 				break
 			//?
-			case 191: chatOnOff(0); break;
+			case 191: chatbox(0); break;
 			//>
-			case 190: chatOnOff(1); break;
+			case 190: chatbox(1); break;
 			//<
-			case 188: chatOnOff(2); break;
+			case 188: chatbox(2); break;
 			//M
-			case 77: chatOnOff(3); break;
+			case 77: chatbox(3); break;
 			//N
-			case 78: chatOnOff(4); break;
+			case 78: chatbox(4); break;
 			//B
-			case 66: chatOnOff(5); break;
+			case 66: chatbox(5); break;
 		}
 		return false;
 	}
+
 	else if(e.altKey) {
-
 		switch(e.which) {
-			//Alt+1
-			case 97:
-				document.getElementById("navHome").getElementsByTagName("a")[0].click()
-				break
-			//Alt+2
-			case 98:
-				document.getElementsByClassName("headerTinymanName")[0].parentNode.click()
-				break
-			//Alt+↑
+			//Alt+↑ : 項目上移
 			case 38:
-				if(msg_notif_i - 1 >= 0 || story_i - 1 >= 0 || group_i - 1 >= 0) {
+				//塗鴉牆或社團貼文
+				if(stage == "wall") {
+					story_index --
+					resStory(story_index, "comment")
+				}
+				//通知或訊息
+				else if(stage == "notif" || stage == "msg") {
+					//上移
+					if(msg_notif_index > 0) {
+						msg_notif_index --
+						$(item).removeClass(hover)
+						$(item[msg_notif_index]).addClass(hover)
+					}
+					//捲動換頁
+					if(msg_notif_index % n_item_shown == 0) {
 
-					if(msg_notif_i % n_shown == 0) {
+						//避免有些container沒有起始值
+						if(container[0].style.top == "")container[0].style.top = "0px" //to avoid empty
 
-						//將container目前高度加上本頁所有子代之高度 (此處的加 畫面就會往上捲)
-						var top = parseInt(container.style.top, 10)
+						//將container目前高度減去本頁所有子代之高度 (此處的減 畫面就會往下捲)
+						var top = parseInt(container[0].style.top, 10)
 						var nowTop = top
-						for(var j=msg_notif_i-n_shown;j<msg_notif_i;j++)
-							nowTop += children[j].clientHeight-1;
+						for(var j=msg_notif_index-n_item_shown;j<msg_notif_index;j++)
+							nowTop += item[j].clientHeight-1;
 						
 						if(nowTop > 0)nowTop = 0
 						
-						container.style.top = nowTop + "px"
+						container[0].style.top = nowTop + "px"
 					}
-
-					msg_notif_i--
-					story_i--
-					group_i--
-					//如果沒有hover_style的設定就是story或group
-					if(hover_style != undefined) {
-						//移除上一個子代hover效果
-						children[msg_notif_i+1].className = children[msg_notif_i+1].className.replace(" "+hover_style, "")
-						//在當前子代加入hover效果
-						children[msg_notif_i].className += " "+hover_style
-					}
-					else if(state == "story") {
-						//期間可能會穿插廣告類子代 因此必須位移
-						var dataft = home.getElementsByClassName("uiStreamStory")[story_i].getAttribute("data-ft")
-						if(dataft != null)
-							while(dataft.length >= 1000)story_i++
-						showStory(story_i)
-					}
-					else if(state == "group") {
-						sidebar.children[group_i+1].getElementsByClassName("item")[0].style.backgroundColor = "#ffffff"
-						sidebar.children[group_i].getElementsByClassName("item")[0].style.backgroundColor = "#eff2f7"
+				}
+				//側邊攔
+				else if(stage == "sidebar") {
+					if(sidebar_index > 0) {
+						sidebar_index --
+						$(sidebar_item).css("background-color", "#ffffff")
+						$(sidebar_item[sidebar_index]).css("background-color", "#eff2f7")
 					}
 				}
 				break
-			//Alt+↓
+			//Alt+↓ : 項目下移
 			case 40:
-				msg_notif_i++
-				story_i++
-				group_i++
-				//如果沒有hover_style的設定就是story
-				if(msg_notif_i != 0 && hover_style != undefined) {
-					//移除上一個子代hover效果
-					children[msg_notif_i-1].className = children[msg_notif_i-1].className.replace(" "+hover_style, "")
-					//在當前子代加入hover效果
-					children[msg_notif_i].className += " "+hover_style
-					//達到需要換頁的數量時
-					if(msg_notif_i % n_shown == 0) {
+				if(stage == "wall") {
+					story_index ++
+					resStory(story_index, "comment")
+				}
+				else if(stage == "notif" || stage == "msg") {
+					//下移
+					if(msg_notif_index < $(item).length) {
+						msg_notif_index ++
+						$(item).removeClass(hover)
+						$(item[msg_notif_index]).addClass(hover)
+					}
+					//捲動換頁
+					if(msg_notif_index % n_item_shown == 0) {
+
 						//避免有些container沒有起始值
-						if(container.style.top == "")container.style.top = "0px" //to avoid empty
+						if(container[0].style.top == "")container[0].style.top = "0px" //to avoid empty
 
 						//將container目前高度減去本頁所有子代之高度 (此處的減 畫面就會往下捲)
-						var top = parseInt(container.style.top, 10)
+						var top = parseInt(container[0].style.top, 10)
 						var nowTop = top
-						for(var j=msg_notif_i-n_shown;j<msg_notif_i;j++)
-							nowTop -= children[j].clientHeight-1;
-
-						container.style.top = nowTop + "px"
+						for(var j=msg_notif_index-n_item_shown;j<msg_notif_index;j++)
+							nowTop -= item[j].clientHeight-1;
+						
+						container[0].style.top = nowTop + "px"
 					}
 				}
-				else if(story_i != 0 && state == "story") {
-					//期間可能會穿插廣告類子代 因此必須位移
-					var dataft = home.getElementsByClassName("uiStreamStory")[story_i].getAttribute("data-ft")
-					if(dataft != null)
-						while(dataft.length >= 1000)story_i++
-					showStory(story_i)
-				}
-				else if(group_i != 0 && state == "group") {
-					//防止超過最愛的總數
-					if(group_i > sidebar.children.length - 1)group_i--;
-					else {
-						sidebar.children[group_i-1].getElementsByClassName("item")[0].style.backgroundColor = "#ffffff"
-						sidebar.children[group_i].getElementsByClassName("item")[0].style.backgroundColor = "#eff2f7"
+				else if(stage == "sidebar") {
+					if(sidebar_index < sidebar_item.length) {
+						sidebar_index ++
+						$(sidebar_item).css("background-color", "#ffffff")
+						$(sidebar_item[sidebar_index]).css("background-color", "#eff2f7")
 					}
 				}
 				break
 		}
 	}
+
 	else if(e.ctrlKey) {
 		switch(e.which) {
 			//Ctrl+↵
 			case 13:
-				//點擊msg跟notif
-				if(document.getElementById("fbNotificationsFlyout").className.search("toggleTargetClosed") == -1 || document.getElementById("fbMessagesFlyout").className.search("toggleTargetClosed") == -1) {
+				//選取notif或msg
+				if (stage == "notif" || stage == "msg") {
 					//使用彈出式視窗
-					children[msg_notif_i].getElementsByTagName("a")[0].target = "_blank"
-					children[msg_notif_i].getElementsByTagName("a")[0].click()
+					$(item[msg_notif_index]).find("a").prop("target", "_blank")
+					$(item[msg_notif_index]).find("a")[0].click()
 				}
-				else if(state == "group") {
-					sidebar.children[group_i].getElementsByClassName("item")[0].click()
+				//選取sidebar item
+				else if(stage == "sidebar") {
+					$(sidebar_item[sidebar_index]).find(".item")[0].click()
 				}
 				break
 			//Ctrl+\
 			case 220:
-				if(state == "story") {
-					home.getElementsByClassName("uiStreamStory")[story_i].getElementsByClassName("UFILikeLink")[0].click()
+				//塗鴉牆貼文按讚
+				if(stage == "wall") {
+					resStory(story_index, "like")
 				}
 				break
 		}
 	}
 };
 
-//每秒鐘偵測是否有子代不足使用者瀏覽的情況 目前皆訂為瀏覽到80%就要繼續擷取
-setInterval(function() {
-	if (document.getElementById("fbNotificationsFlyout").className.search("toggleTargetClosed") == -1) {
-		if((msg_notif_i+1) / document.getElementById("fbNotificationsList").childNodes.length >= 0.8) {
-			document.getElementsByClassName("notifMorePager")[0].getElementsByTagName("a")[0].click()
-		}
-	}
-	else if (document.getElementById("fbMessagesFlyout").className.search("toggleTargetClosed") == -1) {
-		if((msg_notif_i+1) / document.getElementsByClassName("jewelContent")[0].getElementsByTagName("li").length >= 0.8) {
-			document.getElementsByClassName("uiMorePager")[0].getElementsByTagName("a")[0].click()
-		}
-	}
-	else if (state == "story"){
-		setContainer("story")
-		if(home != undefined)
-		if((story_i+1) / home.getElementsByClassName("uiStreamStory").length >= 0.8) {
-			//首頁的更多消息按鈕id與社團的不同
-			if(document.getElementById("pagelet_stream_pager") != null) {
-				document.getElementById("pagelet_stream_pager").getElementsByClassName("uiMorePagerPrimary")[0].click()
-			}
-			else if(document.getElementById("pagelet_group_pager") != null) {
-				document.getElementById("pagelet_group_pager").getElementsByClassName("uiMorePagerPrimary")[0].click()
-			}
-		}
-	}
-}, 1000);
+/* ------------------------------------------------------------------ */
+
+//提示字
+var tip = function(value) {
+	return $("<div>")
+				.text(value)
+				.css("position", "absolute")
+				.css("right", "0px")
+				.css("bottom", "0px")
+				.css("color", "white")
+				.css("font-size", "10px")
+				.css("line-height", "1")
+				.css("background-color", "#3b5998")
+};
+
+$("#navHome").find("a").append("<br />Alt+1").css("lineHeight", "14px")
+$(".headerTinymanName").append("<br />Alt+2").css("lineHeight", "14px")
+$("#fbRequestsJewel").append(tip("Z"))
+$("#fbMessagesJewel").append(tip("X"))
+$("#fbNotificationsJewel").append(tip("C"))
+$("#navSearch").append(tip("V"))
