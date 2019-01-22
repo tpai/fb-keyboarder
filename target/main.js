9032667e81241d4dd0d3e53cdce707d3e61e37c3
Tony Pai <tony77794@gmail.com>
2019-01-22 16:24:44 +0800

    Refactor main and get rid of jq

--- target/main.js
+++ target/main.js
@@ -1,73 +1,91 @@
 if (navigator.appVersion.indexOf('Win') != -1) os = 'win';
 else if (navigator.appVersion.indexOf('Mac') != -1) os = 'mac';
 
-var $home = $('a[data-gt=\'{"chrome_nav_item":"home_chrome"}\']');
-var $timeline = $("a[title='個人檔案']").find('span > span');
+// ---------------- selectors -------------------
+// banner
+const qSearch  = 'input[name="q"]';
+const profile  = 'img[id^="profile_pic_header"]';
+const home     = 'a[data-gt=\'{"chrome_nav_item":"home_chrome"}\']';
+const timeline = 'a[data-gt=\'{"chrome_nav_item":"timeline_chrome"}\']';
+const requests = 'a[name="requests"]';
+const messages = 'a[name="mercurymessages"]';
+const notifs   = 'a[name="notifications"]';
+const creation = 'a#creation_hub_entrypoint';
+const info     = 'a[aria-label="使用說明"]';
 
-// Quick Launch Hint Text
-var tip = function(val) {
-  return `<div id='shortcut-${val}' style='
-      position: absolute;
-      padding: 2px;
-      right: 0px;
-      bottom: 0px;
-      font-size: 10px;
-      line-height: 1;
-      background-color: #3b5998;
-      color: white;
-      z-index: 9;
-    '>
-    ${val}
-    </div>`;
-};
+// ---------------- DOM -------------------
+let $profile;
+let $home;
+let $timeline;
+let $requests;
+let $messages;
+let $notifs;
+let $creation;
+let $info;
 
-if (os == 'win') {
-  $('#fbRequestsJewel').append(tip('Z'));
-  hint1 = 'Ctrl-Alt+U';
-  hint2 = 'Ctrl-Alt+Y';
-} else {
-  hint1 = '⌘-Ctrl+U';
-  hint2 = '⌘-Ctrl+Y';
-}
+window.onload = () => {
 
-$(document).ready(function() {
-  $($('a.jewelButton')[1])
-    .parent()
-    .append(tip('O'));
-  $('#fbNotificationsJewel').append(tip('P'));
-  $('#fbRequestsJewel').append(tip('I'));
-  $("img[id^='profile_pic_header']").css('float', 'left');
-  $timeline
-    .html($timeline.text() + '<br />' + hint2)
-    .css('line-height', '14px');
-  $home.append('<br />' + hint1).css('line-height', '14px');
+  $profile  = q(profile);
+  $home     = q(home);
+  $timeline = q(`${timeline} span > span`);
+  $requests = q(requests);
+  $messages = q(messages);
+  $notifs   = q(notifs);
+  $creation = q(creation);
+  $info     = q(info);
+
+  // Remove unnecessary buttons
+  $creation.remove();
+  $info.remove();
+
+  // ---------------- Hint Letter/Text -------------------
+  let hintTextOfHome;
+  let hintTextOfTimeline;
+  if (os === 'win') {
+    hintTextOfHome     = 'Ctrl-Alt+U';
+    hintTextOfTimeline = 'Ctrl-Alt+Y';
+  } else {
+    hintTextOfHome     = '⌘-Ctrl+U';
+    hintTextOfTimeline = '⌘-Ctrl+Y';
+  }
+
+  $profile.style.float = 'left';
+  $home.style.lineHeight = '14px';
+  $home.innerHTML += '<br />' + hintTextOfHome;
+  $timeline.style.lineHeight = '14px';
+  $timeline.innerHTML = $timeline.textContent + '<br />' + hintTextOfTimeline;
+  $requests.parentNode.appendChild(createHintLetter('I'));
+  $messages.parentNode.appendChild(createHintLetter('O'));
+  $notifs.parentNode.appendChild(createHintLetter('P'));
 
   setInterval(function() {
-    $("input[name='q']").prop('placeholder', '搜尋 ⌘-Ctrl+K');
-    $('#chatsidebarsheet')
-      .next()
-      .find("input[type='text']")
-      .prop('placeholder', '⌘-Ctrl+J');
+    q(qSearch).setAttribute('placeholder', '搜尋 ⌘-Ctrl+K');
+    ['#BuddylistPagelet', '.fbChatSidebar'].map((selector) => {
+      q(selector)
+        .querySelector('#chatsidebarsheet')
+        .nextSibling
+        .querySelector('input[type="text"]')
+        .setAttribute('placeholder', '⌘-Ctrl+J');
+    });
   }, 1000);
-});
 
-// Chatbox Trigger
-var chatbox = function(offset) {
-  var tabs = $('#ChatTabsPagelet div.fbNub');
-  var tab = tabs[tabs.length - offset - 1];
-  if (tab === undefined) return;
-
-  var $tab = $(tab);
-  if ($tab.prop('class').search('opened') != -1)
-    triggerMouseEvent($tab.find("div[role='heading']")[0], 'mouseup');
-  else triggerMouseEvent($tab.find("div[role='button']")[0], 'mouseup');
+  // highlight message entry
+  Array.prototype.map.call(qo('a.messagesContent'), (entry) => {
+    entry.addEventListener('focusin', () => {
+      entry.style.backgroundColor = '#f6f7f9';
+    });
+    entry.addEventListener('focusout', () => {
+      entry.style.backgroundColor = '';
+    });
+  });
 };
 
-var list_mode;
-var list_index = 0;
+let currentMode;
+let currentIndex = 0;
 
 // Keydown Event
 window.onkeydown = function(e) {
+
   if (
     (os == 'win' && e.ctrlKey && e.altKey) ||
     (os == 'mac' && e.metaKey && e.ctrlKey)
@@ -76,109 +94,157 @@ window.onkeydown = function(e) {
     switch (e.which) {
       //Y
       case 89:
-        $('a[title="個人檔案"]')[0].click();
+        triggerMouseEvent($timeline, 'click');
         break;
       //U
       case 85:
-        $("div[data-click='home_icon']")
-          .find('a')[0]
-          .click();
+        triggerMouseEvent($home, 'click');
         break;
       //I
       case 73:
-        $('a.jewelButton')[0].click();
+        triggerMouseEvent($requests, 'click');
         break;
       //O
       case 79:
-        list_mode = 'MercuryJewel';
-        $('a.jewelButton')[1].click();
+        currentMode = 'MercuryJewel';
+        triggerMouseEvent($messages, 'click');
         break;
       //P
       case 80:
-        list_mode = 'fbNotifications';
-        $('a.jewelButton')[2].click();
+        currentMode = 'fbNotifications';
+        triggerMouseEvent($notifs, 'click');
         break;
       //J
       case 74:
-        $('#chatsidebarsheet')
-          .next()
-          .find("input[type='text']")
-          .focus();
+
+        let $chatDialogContainer;
+        let $chatDialogToggler;
+        let $chatDialogTitlebar;
+        $chatDialogContainer = q('div#fbDockChatBuddylistNub');
+        $chatDialogToggler   = q('div#BuddylistPagelet a.fbNubButton');
+        $chatDialogTitlebar  = q('div#BuddylistPagelet div.titlebarLabel');
+
+        // toggle chat dialog
+        if ($chatDialogContainer.className.search('openToggler') === -1) {
+          triggerMouseEvent($chatDialogToggler, 'click');
+        } else {
+          triggerMouseEvent($chatDialogTitlebar, 'click');
+        }
+
+        const chatSidebar = q('div.fbChatSidebar');
+        let parentNodeId;
+        if (
+          chatSidebar === null ||
+          chatSidebar.className.search('hidden_elem') !== -1
+        ) {
+          parentNodeId = '#BuddylistPagelet';
+        } else {
+          parentNodeId = '.fbChatSidebar';
+        }
+        const chatSearch       = q(`${parentNodeId} #chatsidebarsheet`).nextSibling;
+        const chatSearchInput  = chatSearch.querySelector('input[type="text"]');
+        if (chatSearch) {
+          chatSearchInput.focus();
+        }
         break;
       //K
       case 75:
-        $("input[name='q']").focus();
+        q(qSearch).focus();
         break;
       //\/
       case 191:
-        chatbox(0);
+        toggleChatbox(0);
         break;
       //.
       case 190:
-        chatbox(1);
+        toggleChatbox(1);
         break;
       //,
       case 188:
-        chatbox(2);
+        toggleChatbox(2);
         break;
       //M
       case 77:
-        chatbox(3);
+        toggleChatbox(3);
         break;
       //N
       case 78:
-        chatbox(4);
+        toggleChatbox(4);
         break;
       //B
       case 66:
-        chatbox(5);
+        toggleChatbox(5);
         break;
     }
     return false;
   } else if ((os == 'win' && e.altKey) || (os == 'mac' && e.shiftKey)) {
     // message selection
-    if (list_mode == 'MercuryJewel') {
+    if (currentMode == 'MercuryJewel') {
       switch (e.which) {
         // arrow up
         case 38:
-          if (list_index > 0) list_index--;
+          if (currentIndex > 0) currentIndex--;
           break;
         // arrow down
         case 40:
-          list_index++;
+          currentIndex++;
           break;
       }
-      $($('a.messagesContent')[list_index])
-        .focusin(function() {
-          $(this).css('background-color', '#f6f7f9');
-        })
-        .focusout(function() {
-          $(this).css('background-color', '');
-        })
-        .focus();
-    } else if (list_mode == 'fbNotifications') {
+      qo('a.messagesContent')[currentIndex].focus();
+    } else if (currentMode == 'fbNotifications') {
       // notification selection
       switch (e.which) {
         // arrow up
         case 38:
-          if (list_index > 0) list_index--;
+          if (currentIndex > 0) currentIndex--;
           break;
         // arrow down
         case 40:
-          list_index++;
+          currentIndex++;
           break;
       }
-      $("ul[data-testid='react_notif_list'] li div.anchorContainer")
-        .children()
-        .children()
-        .closest('a:first-child')
-        [list_index].focus();
+      qo('a[data-testid="notif_list_item_link"]')[currentIndex].focus();
     }
   }
 };
 
 function triggerMouseEvent(node, eventType) {
-  var clickEvent = document.createEvent('MouseEvents');
+  const clickEvent = document.createEvent('MouseEvents');
   clickEvent.initEvent(eventType, true, true);
   node.dispatchEvent(clickEvent);
+}
+
+function createHintLetter (val) {
+  const div = document.createElement('div');
+  div.setAttribute('id', `shortcut-${val}`);
+  div.setAttribute('style', `
+    position: absolute;
+    padding: 2px;
+    right: 0px;
+    bottom: 0px;
+    font-size: 10px;
+    line-height: 1;
+    background-color: #3b5998;
+    color: white;
+    z-index: 9;
+  `);
+  div.textContent = val;
+  return div;
+}
+
+function toggleChatbox (offset) {
+  const tabs = qo('#ChatTabsPagelet div.fbNub');
+  const tab = tabs[tabs.length - offset - 1];
+
+  if (tab) {
+    triggerMouseEvent(tab.querySelector('div.titlebar').firstChild, 'click');
+  }
+}
+
+function q (selector) {
+  return document.querySelector(selector);
+}
+
+function qo (selector) {
+  return document.querySelectorAll(selector);
 }
